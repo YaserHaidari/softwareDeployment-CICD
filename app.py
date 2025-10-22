8f90d053acad15d8bb79a370669e819658bffd9e
Yaser Haidari <104608220@student.swin.edu.au>
2025-10-22 16:22:21 +1100

    added lint

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask, jsonify, render_template
 
-
 def create_app():
     app = Flask(__name__)
-
 
     @app.route('/')
     def index():
         # Render the HTML UI from templates/index.html
         return render_template('index.html')
 
-
     @app.route('/health')
     def health():
         return jsonify({"status": "ok"}), 200
 
-
     return app
-
 
 if __name__ == '__main__':
     create_app().run(host='0.0.0.0', port=5000, debug=True)
